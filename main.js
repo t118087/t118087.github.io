6b0da166d9ef7b0f274af0a8b4e550bf05b67455
pseuxide <s3pt3mb3r@protonmail.com>
2022-11-25 21:52:45 +0900

    f

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const url = "https://github.com/t118087/t118087.github.io/blob/main/dummydata.js
 
 jQuery(document).ready(function () {
   console.log("on load")
-  fetch("url", { method: "GET" })
+  fetch(url, { method: "GET" })
     .then(res => {
       console.log("fetch returned")
       if (res.ok) {
