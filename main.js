8d99994ec685ad09dc91f7f157ea4ffd7aa9db9c
pseuxide <s3pt3mb3r@protonmail.com>
2022-11-25 21:44:27 +0900

    added main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const url = "https://github.com/t118087/t118087.github.io/blob/main/dummydata.json"
+
+jQuery(document).ready(function () {
+  console.log("on load")
+  fetch("url", { method: "GET" })
+    .then(res => {
+      console.log("fetch returned")
+      if (res.ok) {
+        res.json();
+        for (let i = 0; i < res.data.length; i++) {
+          let buffer = `<li>${res.data[i]}</li>`
+          console.log(buffer)
+          $(".data-store").append(buffer)
+        }
+      }
+    })
+});
